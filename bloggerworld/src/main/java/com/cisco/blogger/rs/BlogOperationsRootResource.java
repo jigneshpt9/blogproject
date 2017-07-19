faeb66a008ad6caee4073deddedac883229a81d3
prpriyad <prpriyad@cisco.com>
2017-07-19 10:34:45 +0530

    Committed JS and bootstrap js

--- bloggerworld/src/main/java/com/cisco/blogger/rs/BlogOperationsRootResource.java
+++ bloggerworld/src/main/java/com/cisco/blogger/rs/BlogOperationsRootResource.java
@@ -1,8 +1,10 @@
 package com.cisco.blogger.rs;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -26,6 +28,24 @@ public class BlogOperationsRootResource {
 		int blogId = blogService.createBlog(blog);
 		
 		return Response.ok().entity(blog).header("location", "/blog/view/" + blogId).build();
+
+		} catch(BlogCreateException bce) {
+			return Response.status(400).build();
+		} catch(BlogException be) {
+			return Response.status(500).build();
+		}
+		
+	}
+	
+	@GET
+	@Path("/view/{blogId}")
+	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	public Response getBlogById(@PathParam("blogId") int blogId) {
+		try {
+		Blog blog = blogService.viewBlog(blogId);
+		
+		return Response.ok().entity(blog).build();
 
 		} catch(BlogCreateException bce) {
 			return Response.status(400).build();
